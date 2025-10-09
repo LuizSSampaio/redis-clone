b6f6eebdfde03e8d8beb6525b2f0fc1615c2b0ce
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-09 19:58:25 -0300

    feat: add TCP server handling with read/write operations

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut buf = [0; 512];
+                stream.read_exact(&mut buf).unwrap();
+
+                println!("received request: {}", String::from_utf8_lossy(&buf[..]));
+
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
