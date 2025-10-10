5bdb530ccfcc548845958ab809a36bab58614fe4
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-10 09:18:29 -0300

    feat: Add command handler for PING and ECHO commands

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,25 @@ async fn process(mut socket: TcpStream) {
         }
 
         let content = resp_parser::parse(&buffer[..bytes_read]);
-        println!("Received: {:?}", content);
-        socket.write_all(b"+PONG\r\n").await.unwrap();
+        let response = command_handler(content);
+        socket.write_all(response.as_bytes()).await.unwrap();
+    }
+}
+
+fn command_handler(command: Vec<String>) -> String {
+    if command.is_empty() {
+        return "-ERR unknown command\r\n".to_string();
+    }
+
+    match command[0].to_uppercase().as_str() {
+        "PING" => "+PONG\r\n".to_string(),
+        "ECHO" => {
+            if command.len() < 2 {
+                "-ERR wrong number of arguments for 'echo' command\r\n".to_string()
+            } else {
+                format!("+{}\r\n", command[1])
+            }
+        }
+        _ => "-ERR unknown command\r\n".to_string(),
     }
 }
