1e75515fbaada5ab1bdcc3606a63c6c5e7e39c39
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-10 08:33:23 -0300

    feat: Migrate TCP server to async/await

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                let mut buffer = [0; 1024];
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
 
-                loop {
-                    let bytes_read = stream.read(&mut buffer).unwrap();
-                    if bytes_read == 0 {
-                        break;
-                    }
+async fn process(socket: TcpStream) {
+    let mut buffer = [0; 1024];
 
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+    loop {
+        let bytes_read = socket.try_read(&mut buffer).unwrap();
+        if bytes_read == 0 {
+            break;
         }
+
+        socket.try_write(b"+PONG\r\n").unwrap();
     }
 }
