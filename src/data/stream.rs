bab8e2432144421cc05a6fe14b04daae0285e29b
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-17 15:46:24 -0300

    feat(data): Add StreamEntryID and error handling for stream IDs

--- src/data/stream.rs
+++ src/data/stream.rs
@@ -1,5 +1,7 @@
 use std::collections::{BTreeMap, HashMap};
 
+use thiserror::Error;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct StreamRecord {
     id: String,
@@ -21,3 +23,25 @@ impl StreamRecord {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct StramValue(BTreeMap<String, HashMap<String, String>>);
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct StreamEntryID {
+    ms: u128,
+    seq: u64,
+}
+
+impl StreamEntryID {
+    pub fn new(source: &str) -> Result<Self, StreamEntryIDError> {
+        todo!()
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum StreamEntryIDError {
+    #[error("Invalid stream ID format")]
+    InvalidFormat,
+    #[error("The ID specified in XADD must be greater than 0-0")]
+    MustBeGreater00,
+    #[error("The ID specified in XADD is equal or smaller than the target stream top item")]
+    EqualOrSmallerThanLastID,
+}
