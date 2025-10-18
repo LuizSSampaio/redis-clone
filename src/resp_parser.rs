bc93a3dbbf280d9e1f37de9db5609dd0cdb6af65
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-18 09:23:51 -0300

    fix(resp_parser): handle single * lines

--- src/resp_parser.rs
+++ src/resp_parser.rs
@@ -33,6 +33,10 @@ pub fn parse(source: &[u8]) -> Vec<String> {
     let mut res = Vec::new();
 
     source.split("\r\n").for_each(|line| {
+        if line == "*" {
+            res.push(line.to_string());
+            return;
+        }
         if line.starts_with('*') || line.starts_with('$') || line.is_empty() {
             return;
         }
